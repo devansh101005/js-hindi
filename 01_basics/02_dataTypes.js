ed2dfe52924cb43473e1e10d4ae149746ac318ed
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-07-25 13:49:45 +0000

    yyy

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello")  // we are using node js nor browser
+
+//code readability should be high
+
+//js mai jyda dattypes nhi hai
+
+let name ="Devansh"  // string
+let age = 18 //number
+let isLoggedIn =false //boolean  //either true or false
+//bigint //not used so frequently
+
+//null//standalone value bhi hai ye //representation of empty value
+//undefined //value is not defined
+
+//symbol //used while using react //helps to find uniqueness
+
+
+//object => 
+
+    console.log(typeof age);  //used to check type 
+    console.log(typeof null);  // it says object
+    
