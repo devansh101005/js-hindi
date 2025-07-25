fa139ac6cb4680a92edc1b3cd767bfd889c12c01
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-07-25 15:54:42 +0000

    uuu

---
+++ 01_basics/05_memory.js
@@ -0,0 +1,11 @@
+//
+//Two types of memory stack and heap
+
+//primitive mai stack use hota hai
+//non primitive mai heap use hoti hai
+
+let myYoutubeName ="hiteshChoudhary"
+
+let anothername=myYoutubeName
+
+//in stack value ki copy hi milta hai 
