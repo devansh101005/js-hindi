4e39732a8ffc7cf97d05bebfa3f531c365461092
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-07-25 14:32:43 +0000

    yyy

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,35 @@ console.log(booleanIsLoggedIn)
 //conversion karte time dhyan rakho
 // "" => false
 // "Devansh"=> true
+
+
+
+//*****************Opeartions */
+let value=3
+let negValue=-value
+console.log(negValue)
+
+str1 ="hello"
+str2="Devansh"
+str3=str1+str2;
+console.log(str3)
+
+
+//
+console.log("1"+2); //12
+console.log(1+"2");  //12
+console.log("1"+2+2); //122
+console.log(1+2+"3");//32
+
+//Read documentation to undderstand this 
+//https://tc39.es/ecma262/multipage  //in this goto abstract operation
+
+let num1 ,num2 ,num3 
+
+num1 =num2=num3 =2+2  //these type of code are not appreciated well 
+
+
+
+//for prefix and postfix go to mdn it will help
+
+
