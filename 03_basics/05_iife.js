711dcdc636c0ea6119b4d38b9bc26843ac96a15d
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-07-27 07:18:28 +0000

    yyy

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,24 @@
+//Immediately invoked function expression
+
+//aisa function jo turant invoke ho jaye 
+//why we needed this function ?
+//kabhi kabhi ham chahte hain ki jais e hi hamaradatabse start ho turnat wo function start ho jye
+//ya global scope se pollution nhi chahiye hame
+
+(function chai() {
+    console.log(`DB CONNECTED`)
+})();  //so  this is how we use iife functions turant jhat se execute
+//aur haan yahan semicolon use krna imp hai kyuki ye code kahan khatam krna hai ye hame js ko btana hoga
+
+//()() // iife ka syntax
+
+//but isko ham arrow function i tarah bhi to likh skte hain
+  ( function aurcode()    {
+    console.log(`DB CONNECTED`);
+  })();
+
+  //can also be 2written as 
+
+  (() =>{
+    console.log(`DB connected`);
+  }) ()
