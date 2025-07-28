229f88c7c8292f2a6876bbacb712142b8ecce129
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-07-28 03:17:34 +0000

    uuu

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,25 @@
+//redux wagera mai switch kaafi kaam aata hai
+//to copy things just downside use shift+alt+down arrow
+const month=3
+ switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+        
+        
+ 
+    default:
+        console.log("default case matched")
+        break;
+ }
+
+ //
