ec1a5d6b737c075d73f7a1594cd0854b71c4ad0a
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-07-29 12:06:12 +0000

    Jjj

---
+++ 05_iteration/02_loops.js
@@ -0,0 +1,11 @@
+// multiple loops hain ...hamare pass 
+//while,do while loop
+
+
+//while
+let index=0;
+while(index<=10) {
+    console.log(`value of index is ${index}`);
+    index=index+2;  
+
+}
